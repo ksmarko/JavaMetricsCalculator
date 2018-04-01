dc545b0f4bafceb8cefbe88fce9e3566ecbffe9d
Ksenia <ks.markodev@gmail.com>
2018-04-01 18:10:19 +0300

    NumberOfFiles java

--- JavaMetricsCalculator/Program.cs
+++ JavaMetricsCalculator/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MetricsCalculator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace JavaMetricsCalculator
 {
@@ -10,6 +12,15 @@ namespace JavaMetricsCalculator
     {
         static void Main(string[] args)
         {
+            string path = "D:/КСЮ/Programs/Projects/Архіви/LeafPic-dev";
+            var list = Metrics.SearchClasses(path);
+
+            foreach (var el in list)
+                Console.WriteLine(el);
+
+            Console.WriteLine("\n\n " + Metrics.NumberOfFiles(path));
+
+            Console.ReadKey();
         }
     }
 }
---
+++ MetricsCalculator/Metrics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricsCalculator
+{
+    public class Metrics
+    {
+        public static int NumberOfPackages(string path)
+        {
+            if (!Directory.Exists(path))
+                throw new DirectoryNotFoundException();
+
+            return 0;
+        }
+
+        public static List<string> SearchClasses(string path)
+        {
+            List<string> files = Directory.GetFiles(path + "/", "*.java").ToList();
+
+            foreach (var directory in Directory.GetDirectories(path + "/"))
+                foreach (var el in SearchClasses(directory + "/"))
+                    files.Add(el);
+
+            return files;
+        }
+
+        public static int NumberOfFiles(string path)
+        {
+            return SearchClasses(path).Count;
+        }
+    }
+}
